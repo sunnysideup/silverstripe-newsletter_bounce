32685315c9446d92d8e058cf132dc6655a0a25fc
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-15 09:39:07 +0000

    BUGFIX
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/newsletter_bounce/trunk@5622 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/email/NewsletterEmailBounceExtension.php
+++ code/email/NewsletterEmailBounceExtension.php
@@ -8,8 +8,8 @@ class NewsletterEmailBounceExtension extends Extension {
 	private static $header_name_to_email = 'X-silverstripe-to-email';
 
 	function updateNewsletterEmail($me){
-		$this->owner->addCustomHeader($this->config()->get("header_name_member_id"), "123");
-		$this->owner->addCustomHeader($this->config()->get("header_name_to_email"), $this->owner->To());
+		$this->owner->addCustomHeader(Config::inst()->get("NewsletterEmailBounceExtension", "header_name_member_id"), "123");
+		$this->owner->addCustomHeader(Config::inst()->get("NewsletterEmailBounceExtension", "header_name_to_email"), $this->owner->To());
 		//From: "From Name" <from_name@ourdomain.com>
 		$this->owner->addCustomHeader("Reply-To", Config::inst()->get("NewsletterBounceTask", "email"));
 		$this->owner->addCustomHeader("Errors-To", Config::inst()->get("NewsletterBounceTask", "email"));
