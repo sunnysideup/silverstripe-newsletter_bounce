18c96eeeb2ec147a4baad1982c0daab91d095d4f
Simon 'Sphere' Erkelens <simon@casa-laguna.net>
2015-04-22 16:55:54 +0200

    Comment out the defaults.

--- _config.php
+++ _config.php
@@ -4,12 +4,12 @@
 //===================---------------- START newsletter_bounce MODULE ----------------===================
 
 //MUST SET, can also be set in a config.yml
-NewsletterBounceTask::setEmail("test@test.com"); // the username for your bounce-address
-NewsletterBounceTask::setPassword('testPassword'); // The password for your bounce-address
-// The novalidate-cert is needed to bypass unofficial certificates!
-NewsletterBounceTask::setServer("{mail.test.com:143/novalidate-cert}"); // must be a valid imap address, note
-NewsletterBounceTask::setErrorName("Content-Type"); // must be a valid imap address
-NewsletterBounceTask::setErrorValue("message/delivery-status"); // must be a valid imap address
+//NewsletterBounceTask::setEmail("test@test.com"); // the username for your bounce-address
+//NewsletterBounceTask::setPassword('testPassword'); // The password for your bounce-address
+//// The novalidate-cert is needed to bypass unofficial certificates!
+//NewsletterBounceTask::setServer("{mail.test.com:143/novalidate-cert}"); // must be a valid imap address, note
+//NewsletterBounceTask::setErrorName("Content-Type"); // must be a valid imap address
+//NewsletterBounceTask::setErrorValue("message/delivery-status"); // must be a valid imap address
 
 //MAY SET
 //NewsletterEmailBounceExtension::set_email("X-email");
